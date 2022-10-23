d03f775416e9678e68d66b70295f649b2a36df63
M-Phoenixx <mchuhan66@gmail.com>
2022-10-23 21:31:48 +0530

    sign up tab added

--- src/Components/Model/LoginModel.js
+++ src/Components/Model/LoginModel.js
@@ -1,21 +1,22 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
 import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
 import './LoginModel.css'
 import { useState } from 'react';
+import { useEffect } from 'react';
+import Login from './Login';
+import Signup from './Signup';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import { Box } from '@mui/system';
 
 export default function FormDialog() {
   const [open, setOpen] = useState(false);
-  const [check, setcheck] = useState(false);
-
-   const change = () =>{
-     setcheck(true)
-   }
+  const [value, setValue] = useState(0);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  
   const handleClickOpen = () => {
     setOpen(true);
   };
@@ -26,12 +27,25 @@ export default function FormDialog() {
 
   return (
     <div  >
+       {/* Button to open Dialog */}
       <Button variant="outlined" onClick={handleClickOpen}>
         Login 
       </Button>
       <Dialog   open={open} onClose={handleClose}>
          <div className="container">
-          
+
+         {/* sign up slider */}
+         <Box sx={{ width: '100%' }}>
+         <Tabs
+      onChange={handleChange}
+      value={value}
+      aria-label="Tabs where selection follows focus"
+      selectionFollowsFocus
+    >
+      <Tab sx={{width : '50%'}} label="Sign up" />
+      <Tab sx={{width : '50%'}} label="Login" />
+    </Tabs>
+    </Box>
          </div>
       </Dialog>
     </div>
