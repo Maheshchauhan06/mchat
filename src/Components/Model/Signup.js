207ab89868724167b68f05c0a505fbe91a724e09
M-Phoenixx <mchuhan66@gmail.com>
2022-10-24 16:46:55 +0530

    Signup page complete

--- src/Components/Model/Signup.js
+++ src/Components/Model/Signup.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Singup.css'
+import {ReactComponent as Google} from '../../assects/Google.svg'
 
 const Signup = () => {
   return (
@@ -10,7 +11,12 @@ const Signup = () => {
      <input type="email" placeholder='E-mail' />
      <input type="password" placeholder='Password' />
      <input type="password" placeholder='Confirm Password' />
-     
+      <button className='signup' >Sign up</button>
+      <div className="break">
+    <p></p> Or Login with <p></p></div>
+    <div className="icon">
+       <Google/>
+      </div>
     </div>
     </>
   )
