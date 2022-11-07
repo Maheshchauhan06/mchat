01baa12d6b9284fbc4efed85c7f9cfff00103bd8
M-Phoenixx <mchuhan66@gmail.com>
2022-11-07 23:30:33 +0530

    register ui done

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import './Register.css'
+import Header from '../Components/header/header'
+import Button from '@mui/material/Button';
 
 const Register = () => {
   return (
-    <>  hlo bro Register now  </>
+    <div className='box'>  <Header/>
+     <div className="contain-register">
+        <input type="text" placeholder="First Name" />
+        <input type="text" placeholder="Last Name" />
+        <input type="Email" placeholder="Email" />
+        <input type="Password" placeholder="Password" />
+        <input type="Password" placeholder="Confirm Password" />
+        <Button sx={{ marginTop:'.5rem', color :'var(--white)', borderColor:"var(--white)", ":hover":{color:'var(--blue)',background:'var(--white)'} }} variant="outlined"> Submit </Button>
+     </div>
+      </div>
   )
 }
 
