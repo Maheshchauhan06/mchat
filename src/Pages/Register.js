b0f01a3b142f0e5d3e348cf02ee6d2ee42f4e68e
M-Phoenixx <mchuhan66@gmail.com>
2022-11-08 14:16:09 +0530

    modular update done

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -6,44 +6,47 @@ import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
 import { auth } from '../firebase';
 
 const Register = () => {
-
-  const [email, setemail] = useState("");
-  const [Password, setPassword] = useState("");
-  const [firstname, setfirstname] = useState("");
-  const [lastname, setlastname] = useState("")
+   const [values, setvalues] = useState({
+     email : '',
+     password : '',
+     firstname : '',
+     lastname : '',   })
 
   const handlesubmit = async (e)=>{
 
 
     console.log("hlo");
     e.preventDefault();
-    const user  = await createUserWithEmailAndPassword(auth,email,Password)
+    const user  = await createUserWithEmailAndPassword(auth,values.email,values.password)
       .then(async(user)=>{
         console.log(user.user);
         await updateProfile(user.user,{
-          displayName: firstname,
+          displayName: values.firstname,
         })
       }).catch((error)=>{
         console.log(error);
       })
       console.log(user);
-
-    setemail("")
-    setfirstname("")
-    setlastname("")
-    setPassword("")
+  
+      setvalues({
+        email : '',
+        password : '',
+        firstname : '',
+        lastname : '',
+      })
+   
   }
    
 
   return (
     <div className='box'>  <Header/>
      <form onSubmit={handlesubmit} className="contain-register">
-        <input value={firstname} onChange={(e)=>setfirstname(e.target.value)} type="text" placeholder="First Name" />
-        <input value={lastname}  onChange={(e)=>setlastname(e.target.value)} type="text" placeholder="Last Name" />
+        <input value={values.firstname} onChange={(e)=>setvalues({...values, firstname: e.target.value})} type="text" placeholder="First Name" />
+        <input  value={values.lastname} onChange={(e)=>setvalues({...values, lastname: e.target.value})} type="text" placeholder="Last Name" />
 
-        <input value={email} type="Email" onChange={(e)=>setemail(e.target.value)} placeholder="Email" />
+        <input value={values.email} type="Email"onChange={(e)=>setvalues({...values, email: e.target.value})}  placeholder="Email" />
 
-        <input value={Password} type="Password"  onChange={(e)=>setPassword(e.target.value)} placeholder="Password" />
+        <input value={values.password} type="Password" onChange={(e)=>setvalues({...values, password: e.target.value})}  placeholder="Password" />
 
         <Button type='submit'  sx={{ marginTop:'.5rem', color :'var(--white)', background:"var(--blue)", ":hover":{color:'var(--blue)',background:'var(--white)'},borderColor:'var(--white)' }} variant="outlined"> Submit </Button>
      </form>
