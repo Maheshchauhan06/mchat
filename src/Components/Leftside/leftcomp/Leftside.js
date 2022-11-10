43e5f7e06d619bc33990dbf82ce4aca18a8e5afd
M-Phoenixx <mchuhan66@gmail.com>
2022-11-10 11:30:25 +0530

    logout

--- src/Components/Leftside/leftcomp/Leftside.js
+++ src/Components/Leftside/leftcomp/Leftside.js
@@ -6,9 +6,14 @@ import SearchIcon from '@mui/icons-material/Search';
 import { auth } from '../../../firebase';
 import Button from '@mui/material/Button';
 import Chats from '../Chats/Chats';
+import { signOut } from 'firebase/auth';
 
 
 const Leftside = ({user}) => {
+
+   const logout = () =>{
+     signOut(auth,user)
+   }
 
 
   return (
@@ -17,7 +22,7 @@ const Leftside = ({user}) => {
     <div className="header">
     <Avatar src= {auth.currentUser?.photoURL} alt = "tera net slow hai" />
       <h3>{auth.currentUser?.displayName  } </h3>
-      <LogoutIcon/>
+      <LogoutIcon onClick={logout} />
     </div>
     <div className="searchbar">
    <div className="searchbox"> <SearchIcon/>
