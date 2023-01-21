3336a0492a03f0fdb4d4fcec26533f0bfca2b6d2
M-Phoenixx <mchuhan66@gmail.com>
2023-01-21 22:18:53 +0530

    updated

--- src/Components/header/header.js
+++ src/Components/header/header.js
@@ -1,21 +1,25 @@
-import React from 'react'
-import './header.css'
-import LoginModel from '../Model/LoginModel'
+import React from "react";
+import "./header.css";
+import LoginModel from "../Model/LoginModel";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <> <div className="contain-header">
-    <a  style={{ textDecoration:'none' }}  href='/'>
-    <h2>Mchat</h2>
-    </a>
-      <div className="leftside">
-      <a style={{ textDecoration:'none' }}  href='/register'>
-      <h2>Register</h2>
-      </a>
-      <LoginModel/></div>
-    </div>
+    <>
+      {" "}
+      <div className="contain-header">
+        <Link style={{ textDecorationLine: "none" }} to={`/`}>
+          <h2>Mchat</h2>
+        </Link>
+        <div className="leftside">
+          <Link style={{ textDecorationLine: "none" }} to={"/register"}>
+            <h2>Register</h2>
+          </Link>
+          <LoginModel />
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
